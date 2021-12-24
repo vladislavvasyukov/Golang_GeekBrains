c6652a6476876af5e440338c316e5bcb101eb48b
vladislavvasyukov <vovasyukov@domclick.ru>
2021-12-24 17:21:17 +0300

    go_1_hw_3

--- Go_Level_1/homework_3/main.go
+++ Go_Level_1/homework_3/main.go
@@ -1,5 +1,67 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math"
+)
 
+func main() {
+	var numberOne, numberTwo float64
+	var operation string
+	fmt.Println("Введите два числа и операцию (Например: 12 + 45):")
+	_, err := fmt.Scan(&numberOne)
+	if err != nil {
+		fmt.Println("Ошибка ввода числа:", err.Error())
+	}
+	_, err = fmt.Scan(&operation)
+	if err != nil {
+		fmt.Println("Ошибка ввода операции:", err.Error())
+	}
+	_, err = fmt.Scan(&numberTwo)
+	if err != nil {
+		fmt.Println("Ошибка ввода числа:", err.Error())
+	}
+	fmt.Println("Результат = ", calculate(numberOne, numberTwo, operation))
+
+	var num int
+	fmt.Println("Введите число:")
+	_, err = fmt.Scan(&num)
+	if err != nil {
+		fmt.Println("Ошибка:", err.Error())
+		return
+	}
+	fmt.Println("Все простые числа меньшие", num)
+	findAllSimpleNumbers(num)
+}
+
+func calculate(numberOne float64, numberTwo float64, operation string) (result float64) {
+	switch operation {
+		case "+":
+			result = numberOne + numberTwo
+		case "-":
+			result = numberOne - numberTwo
+		case "/":
+			result = numberOne / numberTwo
+		case "*":
+			result = numberOne * numberTwo
+		case "**":
+			result = math.Pow(numberOne, numberTwo)
+		//default:
+		//	error("неизвестная операция")
+	}
+	return result
+}
+
+func findAllSimpleNumbers(number int) {
+	for num := 2; num <= number; num++ {
+		isSimple := true
+		for i := 2; i <= int(math.Sqrt(float64(num))); i++ {
+			if num % i == 0 && num != i {
+				isSimple = false
+			}
+		}
+		if isSimple {
+			fmt.Println(num)
+		}
+	}
 }
