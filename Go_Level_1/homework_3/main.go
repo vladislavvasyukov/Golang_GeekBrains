a8af806e2814f1900dbf7721f97abb4d642ce595
vladislavvasyukov <vovasyukov@domclick.ru>
2022-01-02 10:04:09 +0300

    go_1_hw_3

--- Go_Level_1/homework_3/main.go
+++ Go_Level_1/homework_3/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math"
 )
@@ -9,19 +10,18 @@ func main() {
 	var numberOne, numberTwo float64
 	var operation string
 	fmt.Println("Введите два числа и операцию (Например: 12 + 45):")
-	_, err := fmt.Scan(&numberOne)
+	_, err := fmt.Scan(&numberOne, &operation, &numberTwo)
 	if err != nil {
-		fmt.Println("Ошибка ввода числа:", err.Error())
+		fmt.Println("Ошибка ввода:", err.Error())
 	}
-	_, err = fmt.Scan(&operation)
-	if err != nil {
-		fmt.Println("Ошибка ввода операции:", err.Error())
+
+	result, calculateError := calculate(numberOne, numberTwo, operation)
+	if calculateError != nil {
+		fmt.Println("Ошибка:", calculateError.Error())
+		return
 	}
-	_, err = fmt.Scan(&numberTwo)
-	if err != nil {
-		fmt.Println("Ошибка ввода числа:", err.Error())
-	}
-	fmt.Println("Результат = ", calculate(numberOne, numberTwo, operation))
+
+	fmt.Println("Результат = ", result)
 
 	var num int
 	fmt.Println("Введите число:")
@@ -34,22 +34,29 @@ func main() {
 	findAllSimpleNumbers(num)
 }
 
-func calculate(numberOne float64, numberTwo float64, operation string) (result float64) {
+func calculate(numberOne float64, numberTwo float64, operation string) (float64, error) {
+	var result float64
+	var err error
+
 	switch operation {
 		case "+":
 			result = numberOne + numberTwo
 		case "-":
 			result = numberOne - numberTwo
 		case "/":
+			if numberTwo == 0 {
+				err = errors.New("делить на ноль нельзя")
+				break
+			}
 			result = numberOne / numberTwo
 		case "*":
 			result = numberOne * numberTwo
 		case "**":
 			result = math.Pow(numberOne, numberTwo)
-		//default:
-		//	error("неизвестная операция")
+		default:
+			err = errors.New("неизвестная операция")
 	}
-	return result
+	return result, err
 }
 
 func findAllSimpleNumbers(number int) {
@@ -58,6 +65,7 @@ func findAllSimpleNumbers(number int) {
 		for i := 2; i <= int(math.Sqrt(float64(num))); i++ {
 			if num % i == 0 && num != i {
 				isSimple = false
+				break
 			}
 		}
 		if isSimple {
